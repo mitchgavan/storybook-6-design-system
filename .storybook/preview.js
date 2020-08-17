a8d63540fd6ff1b13ce3211e21669913a2b27a0c
Mitch Gavan <mitchgavan@gmail.com>
2020-08-17 12:14:46 +1000

    feat: add decoraters to storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,16 @@
+import React from 'react';
+import { ChakraProvider, CSSReset } from '@chakra-ui/core';
+import theme from '@chakra-ui/theme';
 
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-}
+  actions: { argTypesRegex: '^on[A-Z].*' },
+};
+
+export const decorators = [
+  (Story) => (
+    <ChakraProvider theme={theme}>
+      <CSSReset />
+      <Story />
+    </ChakraProvider>
+  ),
+];
